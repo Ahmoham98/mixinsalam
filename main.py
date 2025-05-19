fdfa2e541b9f2522adf8554c3e5fa9cc76506f58
Ahmoham98 <electricallover45@gmail.com>
2025-05-19 10:10:28 +0330

    includes two more router named mixin_client product which are in order, responsible for handling client to integrate with mixin and handle product requests

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import basalam_client
+from routes import basalam_client, mixin_client, products
 
 
 app = FastAPI()
@@ -7,6 +7,9 @@ app = FastAPI()
 
 
 app.include_router(basalam_client.basalam_client, prefix="/basalam/client", tags=["basalam_client"])
+app.include_router(mixin_client.mixin_client, prefix="/mixin/client", tags=["mixin_client"])
+app.include_router(products.product_router, prefix="/product", tags=["products"])
+
 
 
 @app.get("/")
