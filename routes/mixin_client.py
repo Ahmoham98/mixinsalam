f9476db7dd1a21d30357f9b0e98c90b8d1101f6d
Ahmoham98 <electricallover45@gmail.com>
2025-05-19 10:17:23 +0330

    routes for handling mixin web-application client and add get token dependency to the routes using dependency injection

--- routes/mixin_client.py
+++ routes/mixin_client.py
@@ -1,16 +1,37 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, HTTPException, status
+import requests
 
 mixin_client = APIRouter()
 
+post = "POST"
+get = "GET"
+put = "PUT"
+patch = "PATCH"
+delete = "DELETE"
 
-def store_mxin_url_and_mixin_access_token(mixin_url: str, mixin_access_token: str):
-    mixin_url = mixin_url
-    mixin_access_token = mixin_access_token
-
-
-@mixin_client.post()
-async def create_mixin_access_token_and_url(mixin_url: str, access_token: str):
+@mixin_client.post("/")
+async def create_mixin_access_token_and_url(mixin_url: str, token: str):
+    method=get
+    pk = 1
+    url = f"https://{mixin_url}/api/management/v1/products/{pk}"
+    headers={
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Api-Key {token}'
+    }
+    response = requests.request(method=method, url=url, headers=headers)
     
-    #store mixin url and access_token for global use
-    store_mxin_url_and_mixin_access_token(mixin_url, access_token)
+    if response.status_code == 200:
+        pass
+    else: 
+        raise HTTPException(status_code=404, detail="Invalid data. could be your url or your access token")
+
+    data = {
+        "message": "you are connected successfully!",
+        "mixin-ceredentials": {
+            "mixin_url": mixin_url,
+            "access_token": token
+        }
+    }
+    
+    return data
