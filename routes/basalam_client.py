9dd31f0c57b29be2a8ccb0d6295b592612eac58e
Ahmoham98 <electricallover45@gmail.com>
2025-05-19 10:16:16 +0330

    add get token from header dependency to the routes with dependency injection

--- routes/basalam_client.py
+++ routes/basalam_client.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 import requests
+import json
+from dependencies import AccessTokenBearer
 
+access_token_bearer = AccessTokenBearer()
 
 basalam_client = APIRouter()
 
@@ -59,8 +62,8 @@ async def get_client_access_token():
     method=post
     url='https://auth.basalam.com/oauth/token'
     headers={
-        'Content-Type: application/json',
-        'Accept: application/json'
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
     }
     body={
         "client_id" : client_id,
@@ -71,6 +74,9 @@ async def get_client_access_token():
     response = requests.request(method=method, url=url, headers=headers, data=body)
     
     if response.status_code == 200:
+        
+        response = response.json()
+        
         token_type = response['token_type']
         expire_time = response['expires_in']
         client_access_token = response['access_token']
@@ -88,56 +94,63 @@ async def get_client_access_token():
 
 #read user data from basalam v3/users/me
 @basalam_client.get("/me")
-async def get_my_basalam_data(token: str): #get token from header
+async def get_my_basalam_data(token: str = Depends(access_token_bearer)):
+    
+    if not token: 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="make sure you provided a valid user baerer access token as a value for Authorization header")
+    
     #creating request
     method=get
     url= "https://core.basalam.com/v3/users/me"         #AOuth2 request for getting access-token from basalam
     headers={
-        'Accept: application/json',
-        f'Authorization: Bearer {token}'
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {token}'
     }
     response = requests.request(method=method, url=url, headers=headers)
     
     if response.status_code == 200:
-        pass
+        
+        response = response.json()
+        
+        user_id = response['id']
+        user_hash_id = response['hash_id']
+        username = response['username']
+        name = response['name']
+        
+        vendor_id = response['vendor']['id']
+        vendor_identifier = response['vendor']['identifier']
+        vendor_title = response['vendor']['title']
+        
+        data = {
+            "user": {
+                "id": user_id,
+                "hash_id": user_hash_id,
+                "username": username,
+                "name": name
+            },
+            "vendor": {
+                "id": vendor_id,
+                "identifier": vendor_identifier,
+                "title": vendor_title
+            }
+        }
+        return data
     else:
         return {"message": "problem in fetch user data from basalam. request may have problem here... "}
-    
-    response = response.json()
-    
-    user_id = response['id']
-    user_hash_id = response['hash_id']
-    username = response['username']
-    name = response['name']
-    
-    vendor_id = response['vendor']['id']
-    vendor_identifier = response['vendor']['identifier']
-    vendor_title = response['vendor']['title']
-    
-    data = {
-        "user": {
-            "id": user_id,
-            "hash_id": user_hash_id,
-            "username": username,
-            "name": name
-        },
-        "vendor": {
-            "id": vendor_id,
-            "identifier": vendor_identifier,
-            "title": vendor_title
-        }
-    }
-    return data
 
 
 @basalam_client.get("/verify-my-token")
-async def verify_token(token: str):       #get token from header
+async def verify_token(token: str = Depends(access_token_bearer)):
+    
+    if not token:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="please make sure you provided a valid user bearer access token as a value for Authorization header")
+    
     method = get
     url = "https://auth.basalam.com/whoami"
     headers = {
-        f'Authorization: Bearer {token}',
-        'Content-Type: application/json',
-        'accept: application/json'
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
     }
     response = requests.request(method=method, url=url, headers=headers)
     
