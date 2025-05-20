00431b0eb71cef68195cae97de65213755411e37
Ahmoham98 <electricallover45@gmail.com>
2025-05-20 08:09:08 +0330

    Debug and error handling

--- routes/mixin_client.py
+++ routes/mixin_client.py
@@ -15,23 +15,24 @@ async def create_mixin_access_token_and_url(mixin_url: str, token: str):
     pk = 1
     url = f"https://{mixin_url}/api/management/v1/products/{pk}"
     headers={
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
         'Authorization': f'Api-Key {token}'
     }
+    
     response = requests.request(method=method, url=url, headers=headers)
     
     if response.status_code == 200:
-        pass
-    else: 
-        raise HTTPException(status_code=404, detail="Invalid data. could be your url or your access token")
-
-    data = {
-        "message": "you are connected successfully!",
-        "mixin-ceredentials": {
-            "mixin_url": mixin_url,
-            "access_token": token
+        data = {
+            "message": "you are connected successfully!",
+            "mixin-ceredentials": {
+                "mixin_url": mixin_url,
+                "access_token": token
+            }
         }
-    }
-    
-    return data
+        
+        return data
+    elif response.status_code == 403:
+        raise HTTPException(status_code=403, detail="we can't login with the following credentials")
+    elif response.status_code == 500:
+        raise HTTPException(status_code=500, detail="some error occurred... could be from server or from our request.")
+    else:
+        raise HTTPException(status_code=404, detail="Invalid data. could be your url or your access token")
