24c8a7a1cde05fd7577dd5db04bd47acf88aa6ac
Ahmoham98 <electricallover45@gmail.com>
2025-05-20 08:07:49 +0330

    adds basalam created credentials into the .env file

--- configure.py
+++ configure.py
@@ -5,6 +5,10 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str
+    ID: int
+    REDIRECT_URL: str
+    NAME: str
+    BASALAM_SECRET: str
     
     model_config = SettingsConfigDict(
         env_file=".env",
