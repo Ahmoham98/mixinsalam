4797f5c6ffe3f3bae270a4d380784ca36fe47e14
Ahmoham98 <electricallover45@gmail.com>
2025-05-23 15:44:13 +0330

    debugs get all current user products from basalam and get products with given product_id from basalam routes and update some data for figuring out the basalam api data validation type...

--- routes/user_products.py
+++ routes/user_products.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status, Depends, Form, UploadFile, File
 from sqlalchemy.ext.asyncio.session import AsyncSession
-import requests
-import json
+from dependencies import AccessTokenBearer
 
 from database import get_session
 from schema.mixin import MixinCreate, MixinAddToDatabase
 from schema.basalam import BasalamCreate, BaslaamUpdate
 from controllers.products import ProductController
 
+access_token_bearer = AccessTokenBearer()
 
 post = "POST"
 get = "GET"
@@ -24,9 +24,13 @@ async def get_all_mixin_products(mixin_url: str, mixin_token: str, mixin_page: i
     result = await ProductController.get_mixin_products(url=mixin_url, mixin_token=mixin_token, mixin_page=mixin_page)
     return result
 
-@product_router.get("/my-basalam-products")
-async def get_all_basalam_products(basalam_page: int = 1):
-    result = await ProductController.get_basalam_products(basalam_page=basalam_page)
+@product_router.get("/my-basalam-products/{vendor_id}")
+async def get_all_basalam_products(
+    vendor_id: int,
+    basalam_page: int = 1,
+    token: str = Depends(access_token_bearer),
+):
+    result = await ProductController.get_basalam_products(token=token ,vendor_id=vendor_id ,basalam_page=basalam_page)
     return result
 
 @product_router.get("/is-equal")
@@ -51,11 +55,12 @@ async def get_mixin_product_by_product_id(
 @product_router.get("/basalam/{basalam_product_id}")
 async def get_basalam_product_by_product_id(
     basalam_product_id: int,
+    token: str = Depends(access_token_bearer),
 ):
-    result = await ProductController.get_basalam_product(basalam_product_id=basalam_product_id)
+    result = await ProductController.get_basalam_product(token=token ,basalam_product_id=basalam_product_id)
     return result
 
-@product_router.post("/mixin")
+@product_router.post("/create/mixin")
 async def create_mixin_product(
     *,
     session: AsyncSession = Depends(get_session),
@@ -66,12 +71,14 @@ async def create_mixin_product(
     result = await ProductController.create_mixin_product(url=mixin_url, mixin_token=mixin_token, mixin_body=data)
     return result
 
-@product_router.post("/basalam/{vendor_id}")
+@product_router.post("/create/basalam/{vendor_id}")
 async def create_basalam_product(
     vendor_id: int,
     data: BasalamCreate,
+    photo: UploadFile = File(None),
+    token: str = Depends(access_token_bearer)
 ):
-    result = await ProductController.create_basalam_product(vendor_id=vendor_id, basalam_body=data)
+    result = await ProductController.create_basalam_product(token=token ,vendor_id=vendor_id, basalam_body=data, photo=photo)
     return result
 
 @product_router.put("/mixin/{mixin_product_id}")
@@ -79,9 +86,20 @@ async def update_mixin_product(mixin_url: str, mixin_token: str, mixin_product_i
     result = await ProductController.update_mixin_product(url=mixin_url, mixin_token=mixin_token, mixin_product_id=mixin_product_id, mixin_body=mixin_body)
     return result
 
-@product_router.put("/basalam/{vendor_id}")
-async def update_basalam_product(vendor_id: int, basalam_body: BaslaamUpdate):
-    result = await ProductController.update_basalam_product(vendor_id=vendor_id, basalam_body=basalam_body)
+@product_router.patch("/update/basalam/{product_id}")
+async def update_basalam_product(
+    product_id: int,
+    photo: UploadFile = File(None),
+    name: str = Form(...),
+    price: str = Form(...),
+    token: str = Depends(access_token_bearer),
+):
+    basalam_body = {
+        "name": name,
+        "price": price
+    }
+    
+    result = await ProductController.update_basalam_product(token=token, product_id=product_id ,basalam_body=basalam_body, photo=photo)
     return result
 
 @product_router.delete("/")
