6adfb195264e690c54e63b498738b0b782e7d793
Ahmoham98 <electricallover45@gmail.com>
2025-05-23 15:36:34 +0330

    adds an "s" to the end of the products_router prefix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.include_router(basalam_client.basalam_client, prefix="/basalam/client", tags=["basalam_client"])
 app.include_router(mixin_client.mixin_client, prefix="/mixin/client", tags=["mixin_client"])
-app.include_router(user_products.product_router, prefix="/product", tags=["products"])
+app.include_router(user_products.product_router, prefix="/products", tags=["products"])
 
 
 
