ac3a5e95d666ce6d55ff9ed7ad09abbc874fb3e7
Ahmoham98 <electricallover45@gmail.com>
2025-05-28 13:16:33 +0330

    corrects middleware miss order problem

--- middleware.py
+++ middleware.py
@@ -13,6 +13,18 @@ logger.disabled = True
 
 def register_middleware(app: FastAPI):
     
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173", "https://mixinsalam.liara.run"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials= True,
+    )
+    
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts = ["*"]
+    )
     
     @app.middleware('http')
     async def custom_logging(request: Request, call_next):
@@ -25,17 +37,3 @@ def register_middleware(app: FastAPI):
         print(message)
         
         return response 
-    
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["http://localhost:5173", "https://mixinsalam.liara.run"],
-        allow_methods=["*"],
-        allow_headers=["*"],
-        allow_credentials= True,
-    )
-    
-    
-    app.add_middleware(
-        TrustedHostMiddleware,
-        allowed_hosts = ["*"]
-    )
