2c79eae4efa0c74ce4ceb2e5660329c03096e5c7
Ahmoham98 <electricallover45@gmail.com>
2025-05-29 05:20:48 +0330

    Removes callback endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import basalam_client, mixin_client, user_products, callback
+from routes import basalam_client, mixin_client, user_products
 import uvicorn
 
 app = FastAPI()
@@ -7,7 +7,6 @@ app = FastAPI()
 
 
 app.include_router(basalam_client.basalam_client, prefix="/basalam/client", tags=["basalam_client"])
-app.include_router(callback.callback_router, prefix="/basalam", tags=["callback"])
 app.include_router(mixin_client.mixin_client, prefix="/mixin/client", tags=["mixin_client"])
 app.include_router(user_products.product_router, prefix="/products", tags=["products"])
 
