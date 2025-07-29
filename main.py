2b9050cd6c7a2026677f3e9d9e1f28cdf0937993
javad <javadazizii2002@gmail.com>
2025-07-29 16:08:49 +0330

    Update server port to 7000 in main.py

--- main.py
+++ main.py
@@ -64,5 +64,5 @@ async def get_root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=7000, reload=False)
 
